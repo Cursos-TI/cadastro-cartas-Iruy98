4bd51fca4caab6b73efd10b8406ba6370baf7098
Yuri <165326514+Iruy98@users.noreply.github.com>
2026-02-24 21:31:00 -0300

    Refactor input/output for city cards in C
    Adicionei operadores para calcular a densidade populacional e o PIB per capta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-   char estado[50], estado2[50], cod[50], cod2[50], cidade[50], cidade2[50];
+int main (){
+    
+    char estado[50], estado2[50], cod[50], cod2[50], cidade[50], cidade2[50];
     int pop, pop2, pt, pt2;
-    float area, area2, pib, pib2;
- // Área para entrada de dados
-   printf ("== Cadastre sua primeira Carta ==\n\n");
+    float area, area2, pib, pib2, denspop1, denspop2, pibpc1, pibpc2;
+
+    printf ("== Cadastre sua primeira Carta ==\n\n");
 
     //carta 1
 
     printf ("Estado:  ");
     scanf  (" %s", &estado);
-    printf("Codigo:  ");
+    printf("codigo:  ");
     scanf (" %s", &cod);
-    printf ("Cidade:  ");
+    printf ("cidade:  ");
     scanf (" %s", &cidade);
     printf ("Populacao: ");
     scanf (" %d", &pop);
@@ -28,29 +23,13 @@ int main() {
     scanf (" %f", &area);
     printf ("PIB: ");
     scanf("%f", &pib);
+
+    //Operador pra densidade e renda per capta 
   
-printf ("\n\n== Cadastre sua segunda Carta ==\n\n");
+    denspop1 = pop / area;
+    pibpc1 = (pib * 1000000)/ pop;
 
-    //carta 2
-
-    printf ("Estado:  ");
-    scanf  (" %s", &estado2);
-    printf("Codigo:  ");
-    scanf (" %s", &cod2);
-    printf ("Cidade:  ");
-    scanf (" %s", &cidade2);
-    printf ("Populacao: ");
-    scanf (" %d", &pop2);
-    printf ("Pontos turisticos: ");
-    scanf (" %d", &pt2);
-    printf ("Area por KM: ");
-    scanf (" %f", &area2);
-    printf ("PIB: ");
-    scanf("%f", &pib2);
-  
-  // Área para exibição dos dados da cidade
-
-  // imprimir resultados da primeira carta 
+    // imprimir resultados da primeira carta 
 
     printf ("\n\n == Sua Carta tem == \n\n");
 
@@ -60,10 +39,41 @@ printf ("\n\n== Cadastre sua segunda Carta ==\n\n");
     printf ("Sua cidade é: %s \n", cidade);
     printf ("O Numero de população é: %d \n", pop);
     printf ("A cidade tem %d de pontos turisticos \n", pt );
-    printf ("Sua cidade tem %f quadrados \n", area);
-    printf ( "O PIB da sua cidade é de: %f \n\n", pib);
+    printf ("Sua cidade tem %.2f km² \n", area);
+    printf ("O PIB da sua cidade é de: %.2f \n", pib);
+    printf ("A densidade populacional é de: %.2f \n", denspop1);
+    printf ("O PIB Percapta é de: %.2f \n\n", pibpc1);
+    
+    
+        //dados para segunda carta
+    
 
-  // imprimir resultados da segunda carta 
+    printf ("== Cadastre sua segunda Carta ==\n\n");
+
+    //carta 2
+
+    printf ("Estado:  ");
+    scanf  (" %s", &estado2);
+    printf("codigo:  ");
+    scanf (" %s", &cod2);
+    printf ("cidade:  ");
+    scanf (" %s", &cidade2);
+    printf ("Populacao: ");
+    scanf (" %d", &pop2);
+    printf ("Pontos turisticos: ");
+    scanf (" %d", &pt2);
+    printf ("Area por KM: ");
+    scanf (" %f", &area2);
+    printf ("PIB: ");
+    scanf("%f", &pib2);
+
+    //operador apra densidade e pib per capta
+
+    denspop2 = pop2 / area2;
+    pibpc2 = (pib2 *1000000) / pop2;
+
+
+    // imprimir resultados da segunda carta 
 
     printf ("\n\n == Sua Carta tem == \n\n");
 
@@ -73,10 +83,12 @@ printf ("\n\n== Cadastre sua segunda Carta ==\n\n");
     printf ("Sua cidade é: %s \n", cidade2);
     printf ("O Numero de população é: %d \n", pop2);
     printf ("A cidade tem %d de pontos turisticos \n", pt2 );
-    printf ("Sua cidade tem %f metros quadrados \n", area2);
-    printf ( "O PIB da sua cidade é de: %f \n\n", pib2);
+    printf ("Sua cidade tem %.2f km² \n", area2);
+    printf ("O PIB da sua cidade é de: %.2f \n", pib2);
+    printf ("A densidade populacional é de: %.2f \n", denspop2);
+    printf ("O PIB Percapta é de : %.2f \n\n", pibpc2);
 
-  
+    
 
-return 0;
-} 
+    return 0;
+}
